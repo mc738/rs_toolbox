c425b2db2d49c7e8e5c4d986248c1f6256c352a9
Max <me@mclifford.dev>
2025-05-15 23:47:16 +0100

    feat(utils): Add utils
    Add parsing.rs to utils

---
+++ src/utils/parsing.rs
@@ -0,0 +1,10 @@
+﻿
+pub(crate) fn is_char(actual: char, excepted: char) -> bool {
+     actual == excepted
+}
+
+pub(crate) fn is_chars(actual: Vec<char>, expected: Vec<char>) -> bool {
+    actual == expected
+}
+
+//pub(crate) fn read
